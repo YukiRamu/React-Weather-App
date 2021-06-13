1d69f1059d89a88f483c8db538ab8c565daead82
Yuki <yukipooh4120@gmail.com>
2021-06-12 17:02:16 -0700

    style done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { React, useContext } from 'react';
+import { React, useContext, useState } from 'react';
 import "./Header.css";
 import { Row, Nav } from 'react-bootstrap';
 import Search from "../Search/Search";
@@ -9,6 +9,42 @@ import { WeatherContext } from '../../Context/WeatherContext';
 const Header = () => {
 
   const { unit, setUnit, setIsUnitChanged } = useContext(WeatherContext);
+  // const [style, setCheckedStyle] = useState({
+  //   "backgroundColor": " #d66c05",
+  //   "backgroundColor": "transparent",
+  //   "color": "#0e2532"
+  // });
+
+  const defaultStyleMetric = {
+    "border": "0.1rem solid #d66c05",
+    "backgroundColor": "#d66c05"
+  };
+
+  const defaultStyleFahrenheit = {
+    "border": "0.1rem solid white",
+    "backgroundColor": "transparent"
+  };
+
+  const checkedStyle = "background-color: #d66c05; border: 0.1rem solid #d66c05";
+
+  const uncheckedStyle = "background-color: transparent; border: 0.1rem solid white";
+
+  const handleChange = (e) => {
+    console.log(e);
+    setUnit(e.target.value);
+    setIsUnitChanged(true);
+    // e.target.checked
+    if (e.target.checked === true) {
+      e.target.parentElement.attributes[1].value = checkedStyle;
+      if (e.target.value === "imperial") {
+        e.target.parentElement.previousSibling.attributes[1].value = uncheckedStyle;
+      } else {
+        e.target.parentElement.nextSibling.attributes[1].value = uncheckedStyle;
+      }
+    }
+  };
+
+
 
   return (
     <>
@@ -19,20 +55,20 @@ const Header = () => {
               <TiWeatherSnow></TiWeatherSnow> Weather Report
             </Link>
             <div className="unit">
-              <label className="unitBtn">
+              <label className="unitBtn celcius" style={defaultStyleMetric}>
                 <input
                   type="radio"
                   value="metric"
-                  onChange={e => { setUnit(e.target.value); setIsUnitChanged(true); }}
+                  onChange={handleChange}
                   checked={unit === "metric"} />
                 Celcius
               </label>
 
-              <label className="unitBtn">
+              <label className="unitBtn metric" style={defaultStyleFahrenheit}>
                 <input
                   type="radio"
                   value="imperial"
-                  onChange={e => { setUnit(e.target.value); setIsUnitChanged(true); }}
+                  onChange={handleChange}
                   checked={unit === "imperial"} />
                 Fahrenheit
               </label>
